8eeca7993e9624e34eef3842c92b48edb4241867
KeinLuka <1875pe18@gmail.com>
2019-12-22 15:02:49 +0300

    js

---
+++ source/js/form.js
@@ -0,0 +1,12 @@
+var form = document.querySelector(".form");
+var submitForm = form.querySelector(".form__btn");
+
+
+/*var formPopap = document.querySelector(".container-form");
+  var form = formPopap.querySelector("form");
+  var formInputs = formPopap.querySelectorAll("input");
+  var openForm = document.querySelector(".about-company__link-contact");
+  var formFocusInput = formPopap.querySelector(".name-iput");
+  var formEmail = formPopap.querySelector(".name-email");
+  var closeForm = formPopap.querySelector(".popup__close");
+  var submissionForm = formPopap.querySelector(".background-button__btn");*/
