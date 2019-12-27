008c24d38cbe45103f6b3bcba226ec69f4c5d3ae
KeinLuka <1875pe18@gmail.com>
2019-12-27 21:51:13 +0300

    слайдер

--- source/js/form.js
+++ source/js/form.js
@@ -1,12 +1,105 @@
 var form = document.querySelector(".form");
-var submitForm = form.querySelector(".form__btn");
+var submitFormButton = form.querySelector(".form__btn");
+var nameInput = form.querySelector("#name-cat");
+var weightInput = form.querySelector("#weight-cat");
+var mailInput = form.querySelector("#master__e-mail");
+var phoneInput = form.querySelector("#master__phone");
+var mailRegEx = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+var mailInputRegEx = /[А-я]/g;
+var numberRegEx = /[^\d]/g;
+form.noValidate = true;
 
+var testMail = function () {
+  mailInput.value = mailInput.value.replace(mailInputRegEx, "");
+};
 
-/*var formPopap = document.querySelector(".container-form");
-  var form = formPopap.querySelector("form");
-  var formInputs = formPopap.querySelectorAll("input");
-  var openForm = document.querySelector(".about-company__link-contact");
-  var formFocusInput = formPopap.querySelector(".name-iput");
-  var formEmail = formPopap.querySelector(".name-email");
-  var closeForm = formPopap.querySelector(".popup__close");
-  var submissionForm = formPopap.querySelector(".background-button__btn");*/
+var testPhone = function () {
+  phoneInput.value = phoneInput.value.replace(numberRegEx, "");
+};
+
+var testWeight = function () {
+  weightInput.value = weightInput.value.replace(numberRegEx, "");
+};
+
+var removeErrorStyle = function (evt) {
+  evt.target.style = null;
+};
+
+var nameInputChangeHandler = function (evt) {
+  removeErrorStyle(evt);
+};
+
+var weightInputChangeHandler = function (evt) {
+  removeErrorStyle(evt);
+  testWeight();
+};
+
+var mailInputChangeHandler = function (evt) {
+  removeErrorStyle(evt);
+  testMail();
+};
+
+var phoneInputChangeHandler = function (evt) {
+  removeErrorStyle(evt);
+  testPhone();
+};
+
+var checkNameInputValidity = function () {
+  var flag = true;
+  if (nameInput.value === "") {
+    flag = false;
+  }
+  return flag;
+};
+
+var checkWeightInputValidity = function () {
+  var flag = true;
+  if (weightInput.value === "") {
+    flag = false;
+  }
+  return flag;
+};
+
+var checkMailInputValidity = function () {
+  var flag = true;
+  if (!mailRegEx.test(mailInput.value)) {
+    flag = false;
+  }
+  return flag;
+};
+
+var checkPhoneInputValidity = function () {
+  var flag = true;
+  if (phoneInput.value === "" || phoneInput.value.length < 6) {
+    flag = false;
+  }
+  return flag;
+};
+
+var checkInputsValidity = function () {
+  if (!checkNameInputValidity()) {
+    nameInput.style.boxShadow = "0 0 3px red";
+  }
+  if (!checkWeightInputValidity()) {
+    weightInput.style.boxShadow = "0 0 3px red";
+  }
+  if (!checkMailInputValidity()) {
+    mailInput.style.boxShadow = "0 0 3px red";
+  }
+  if (!checkPhoneInputValidity()) {
+    phoneInput.style.boxShadow = "0 0 3px red";
+  }
+};
+
+var formSubmitHendler = function (evt) {
+  checkInputsValidity();
+  if (!checkNameInputValidity() || !checkWeightInputValidity() || !checkMailInputValidity() || !checkPhoneInputValidity()) {
+    evt.preventDefault();
+  }
+};
+
+nameInput.addEventListener("input", nameInputChangeHandler);
+weightInput.addEventListener("input", weightInputChangeHandler);
+mailInput.addEventListener("input", mailInputChangeHandler);
+phoneInput.addEventListener("input", phoneInputChangeHandler);
+form.addEventListener("submit", formSubmitHendler);
